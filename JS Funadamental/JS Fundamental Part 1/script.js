0777dacee85ebc0154ae53f8704f060ac169778a
Zoc <zocnishant@gmail.com>
2023-07-13 23:34:07 +0545

    html and js

---
+++ JS Funadamental/JS Fundamental Part 1/script.js
@@ -0,0 +1,88 @@
+/*
+// VALUES AND VARIABLES
+
+let firstName = "Nishant";
+let lastName = "Joshi";
+console.log(firstName);
+console.log(lastName);
+
+// naming a variable
+let n_joc = "NJ";
+let $function = 27;
+
+let myFirstJob = "IT Officer";
+let myCurrentJob = "Software Engineer";
+*/
+
+// Assignment
+//Assignment 1
+const country = "Nepal";
+const continent = "Asia";
+let population = 30300000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Assignment 2
+const isIsland = false;
+const language = "Nepali";
+console.log(typeof isIsland);
+console.log(typeof populaation);
+console.log(typeof country);
+console.log(typeof language);
+
+
+
+/*
+// PRIMITIVE DATA TYPE
+
+let isJSFun = true;
+let age = 24;
+let isCountry = "Nepal";
+console.log(typeof(isJSFun));
+console.log(typeof(age));
+console.log(typeof(isCountry));
+
+isJSFun = "Nepal";  //dynamic typing
+console.log(typeof isJSFun);
+
+//undefined
+let emptyValue;
+console.log(emptyValue);
+console.log(typeof emptyValue);
+
+emptyValue = 2080;
+console.log(emptyValue);
+console.log(typeof emptyValue);
+
+console.log(typeof null)
+*/
+
+// let, const, var
+
+//Arithmetic operator
+
+let now = 2037;
+
+const ageParas = now - 1997;
+const ageSarah = now - 2018;
+console.log(ageParas, ageSarah);
+
+console.log(ageParas * ageSarah, ageParas / 5, 4 ** 2);
+// ** => exponentation
+
+const firstNaam = "Nishant";
+const lastNaam = "Joshi";
+console.log(firstNaam + " " + lastNaam);
+
+//Assignment operator
+let x = 10 + 5;
+x += 15; // x = x + 15
+x--;
+x++
+console.log(x);
+
+//comparision operator
+
+console.log(ageParas > ageSarah);
+console.log(ageSarah >= 18);
