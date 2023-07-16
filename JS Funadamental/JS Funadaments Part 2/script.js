e44f15de9d9169afe990b9dd622d7c5fa725c3e5
Zoc <zocnishant@gmail.com>
2023-07-16 22:35:36 +0545

    Basics

--- JS Funadamental/JS Funadaments Part 2/script.js
+++ JS Funadamental/JS Funadaments Part 2/script.js
@@ -136,7 +136,176 @@ console.log(ages);
 */
 
 //Array Methods
-
+/*
 const friends = ['N', 'I', 'S', 'H'];
 friends.push('A');
-console.log(friends)
+console.log(friends);
+*/
+
+// Obeject 
+/*
+const NISHANT = {
+    firstName: 'Nishant',
+    lastName: 'Joshi',
+    age: 2023 - 1998,
+    job: 'Software Engineer',
+    friends: ['n','i','s'],
+}
+
+console.log(NISHANT);
+
+console.log(NISHANT.firstName);
+console.log(NISHANT['lastName'])
+
+const nameKey = 'Name';
+console.log(NISHANT['first' + nameKey])
+console.log(NISHANT['last' + nameKey])
+
+const interestedIn = prompt('What do you want to know about me?');
+
+if(NISHANT[interestedIn]) {
+    console.log(NISHANT[interestedIn]);
+}else {
+    console.log('Property not found.')
+}
+
+NISHANT.location = 'Kathmandu';
+NISHANT['fb'] = '@paras';
+console.log(NISHANT);
+
+const guff = `${NISHANT.firstName} has 3 friends, and best friend is ${NISHANT.friends[0]}`;
+console.log(guff);
+*/
+
+// Object methods
+/*
+const NISHANT = {
+    firstName: 'Nishant',
+    lastName: 'Joshi',
+    birthYear: 1998,
+    job: 'Software Engineer',
+    friends: ['n','i','s'],
+    hasDriverLicense: true,
+    
+    // calcAge: function(birthYeah) {
+    //     return 2023 - birthYeah;
+    // }
+
+    // calcAge: function() {
+    //     console.log(this);
+    //      return 2023 - this.birthYear;
+    // }
+
+    calcAge: function() {
+        this.age = 2023 - this.birthYear;
+        return this.age;
+    },
+
+    getSummary: function() {
+        return `${this.firstName}`
+    },
+}
+
+
+console.log(NISHANT.calcAge());
+console.log(NISHANT.age);
+
+console.log(NISHANT.getSummary());
+*/
+
+// For Loop
+/*
+//for loop keeps running while the condition is true.
+for(let rep = 1; rep <= 10; rep++) {
+    console.log(`HI ${rep}`);
+}
+*/
+/*
+const NishantArray = [
+    'Nishant',
+    'Joshi',
+    2023 - 1997,
+    'Software  Engineer',
+    ['N', 'I', 'S', 'H'],
+    'Paras',
+];
+
+const types = [];
+
+for (let i = 0; i < NishantArray.length ;i++) {
+    console.log(NishantArray[i], typeof NishantArray[i]);
+
+    types[i] = typeof NishantArray[i];
+}
+
+console.log(types);
+
+const yrs = [1992, 1994, 1997, 1998];
+const age =[];
+
+for (let i = 0; i < yrs.length; i++) {
+    age.push(2023 - yrs[i]);
+}
+
+console.log(age); 
+
+// continue and break
+console.log('-----ONLY STRINGS------');
+
+for (let i = 0; i < NishantArray.length ;i++) {
+    if(typeof NishantArray[i] !== 'string') continue;
+    
+    console.log(NishantArray[i], typeof NishantArray[i]);
+}
+
+console.log('-----BREAK------');
+for (let i = 0; i < NishantArray.length ;i++) {
+    if(typeof NishantArray[i] === 'number') break;
+
+    console.log(NishantArray[i], typeof NishantArray[i]);
+}
+*/
+/*
+const Nish = [
+    'Nishant',
+    'Joshi',
+    2023 - 1998,
+    'Software',
+    ['n', 'i', 's', 'h'],
+    true
+]
+
+for(let i = Nish.length; i >= 0; i-- ) {
+    console.log(Nish[i]);
+}
+
+
+for(let exercise = 1; exercise <= 3; exercise++) {
+    console.log(`-----startring-------- ${exercise} excrise.`);
+
+    for(let rep = 1; rep < 6; rep++) {
+        console.log(`${rep}`);
+    }
+}
+*/
+
+// While loop
+/*
+let rep = 1;
+while (rep <= 10) {
+    console.log(`${rep}`);
+    rep++;
+}
+*/
+
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+
+    if(dice === 6) {
+        console.log("END");
+    }
+}
